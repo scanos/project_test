e53372ccab9c0eae49e2933b697e5c9060b8db8b
scanos <seamuskane@aim.com>
2018-04-25 13:55:21 +0100

    Update add_project.php

--- add_project.php
+++ add_project.php
@@ -1,10 +1,20 @@
-
 <?php
 
 require "../../pdo.php";
+echo $_POST['ops_permit'];
+echo $_SESSION['ops_permit'];
 if (strlen($_SESSION['user']) <1){header("Location: login.php");}
 
-//echo "resln".$_POST['resolution'];
+if ($_POST['ops_permit'] != $_SESSION['ops_permit'])
+{
+$_SESSION['notification'] = " ERROR 101 INVALID ADDITION OPERATION";
+header("Location: display_tasks.php");
+
+}
+
+else
+
+{
 $temp_user= $_SESSION['user'];
 
 $stmt = $pdo->prepare("INSERT INTO ".$projects_table." (description,resolution,owner) VALUES  (:description, :resolution, :owner)");
@@ -17,7 +27,7 @@ $stmt->bindParam(':resolution', $_POST['resolution'], PDO::PARAM_STR);
 
 $stmt->execute();
 
-
+}
 
 
 
